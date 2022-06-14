7522ea2123a2789bba42f873456494ed288a244b
Welldan Yogia <welldanyogia01@gmail.com>
2022-06-14 23:04:53 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ import java.lang.ArrayIndexOutOfBoundsException;
 public class Main {
     public static void main(String[] args){
         int index=0;
-        DinasPertanahan[] dinasPertanahan = new DinasPertanahan[index];
-
-        for (index = 0;; index++) {
-            try {
+        DinasPertanahan[] dinasPertanahan = new DinasPertanahan[2];
+        try {
+            for (index = 0;; index++) {
                 dinasPertanahan[index]=new DinasPertanahan();
                 System.out.printf("\n\nDATA KE-%d\n",index+1);
                 dinasPertanahan[index].setAlamat();
@@ -17,9 +16,11 @@ public class Main {
                 dinasPertanahan[index].getAlamat(index);
                 dinasPertanahan[index].getPanjangTanah(index);
                 dinasPertanahan[index].getLuasTanah(index);
-            }catch (ArrayIndexOutOfBoundsException | InputMismatchException e) {
-                System.err.println(e);
             }
+        }catch (ArrayIndexOutOfBoundsException | InputMismatchException e) {
+            System.err.println(e);
         }
-}
+
+
+        }
 }
